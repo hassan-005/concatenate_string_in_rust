96e5356a36525f2d0831a8c4a79cc3b70fec8cd3
hassan-005 <161137636+hassan-005@users.noreply.github.com>
2024-02-25 21:46:06 +0500

    Delete Rust/target directory

--- Rust/target/debug/deps/rust.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Qadri Laptop\Rust\target\debug\deps\rust.exe: src\main.rs
-
-C:\Users\Qadri Laptop\Rust\target\debug\deps\rust.d: src\main.rs
-
-src\main.rs:
--- Rust/target/debug/rust.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Qadri\ Laptop\Rust\target\debug\rust.exe: C:\Users\Qadri\ Laptop\Rust\src\main.rs
